f4a012121380c879ed58208602de4145d828921a
tapader13 <minhajtapader0@gmail.com>
2024-10-22 22:38:13 -0400

    add refresh to reload auth 6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <body className={`${dm_sans.className} antialiased`}>
-        <SessionProvider>
+        <SessionProvider session={session}>
           <FactoriesProvider propsData={session}>
             {' '}
             <StickyTop />
